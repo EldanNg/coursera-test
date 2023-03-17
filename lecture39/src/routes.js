483c337f7ad3cc5bbf9cf13a43c60c703230b0b4
Eldan <adopteldan@gmail.com>
2023-03-17 22:59:44 +0800

    Lecture 39

---
+++ lecture39/src/routes.js
@@ -0,0 +1,51 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingList')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/shoppinglist/templates/home.template.html'
+  })
+
+  // Premade list page
+  .state('mainList', {
+    url: '/main-list',
+    templateUrl: 'src/shoppinglist/templates/main-shoppinglist.template.html',
+    controller: 'MainShoppingListController as mainList',
+    // Resolve property helps to retrieve the data first before the view comes into browser as it is not best practice to switch view without knowing before if there is any data in the view to begin with. If the promise in the resolve property fails to resolve, router will not transit to the new state at all. This name of the key like the one below 'items' has to be injected into the corresponding controller's function.
+    resolve: {
+      items: ['ShoppingListService', function (ShoppingListService) {
+        return ShoppingListService.getItems();
+      }]
+    }
+  })
+
+  .state('itemDetail', {
+    url: '/item-detail/{itemId}',
+    templateUrl: 'src/shoppinglist/templates/item-detail.template.html',
+    controller: 'ItemDetailController as itemdetail',
+    resolve: {
+      item: ['$stateParams', 'ShoppingListService',
+      function ($stateParams, ShoppingListService) {
+        return ShoppingListService.getItems()
+          .then(function (items) {
+            return items[$stateParams.itemId];
+          });
+      }]
+    }
+  })
+}
+
+})();
