14cae22b21a5ba61814e1d4b885e427368e9e399
Eldan <adopteldan@gmail.com>
2023-03-21 13:17:53 +0800

    Lecture 53

---
+++ lecture53/src/common/common.module.js
@@ -0,0 +1,13 @@
+(function () {
+"use strict";
+
+angular.module('common', [])
+.config(config);
+
+config.$inject = ['$httpProvider'];
+function config($httpProvider) {
+  // The http provider has a special property called interceptors that holds an array of all these interceptors that when the http service goes out to do its job, it first checks that array to see if it needs to have one of the interceptors work its magic on one of the requests or responses before actually goes through with the request.
+  $httpProvider.interceptors.push('loadingHttpInterceptor');
+}
+
+})();
